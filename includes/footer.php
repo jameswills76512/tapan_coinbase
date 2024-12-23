d17b86387d18aca0bf2bbc23501fe2f04a938b90
vinay sachan <vinay@tappetbox.com>
2024-12-24 03:58:33 +0530

    cahnegs

--- includes/footer.php
+++ includes/footer.php
@@ -19,19 +19,6 @@
     (function () {
         var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
         s1.async = true;
-        s1.src = 'https://embed.tawk.to/675489894304e3196aee6aea/1ieh3iha3';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-    })();
-</script>
-
-
-<script type="text/javascript">
-    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-    (function () {
-        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
         s1.src = '<?=TAWK_URL?>';
         s1.charset = 'UTF-8';
         s1.setAttribute('crossorigin', '*');
